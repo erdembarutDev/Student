fd99b24181b08bb2c169f69d6b779342e79a2573
Erdem Barut <erdembarut1641@gmail.com>
2025-09-28 01:15:30 +0300

    Student Grade Calculation
    Kullanıcıdan Öğrenci sayısı alır ve bu yası kaçsa tüm derslerinin ortalamasını alıp dönem sonu sınıfı geçip geçmedigini , geçdiysede sorumlulukdan mı yoksa doğrudanmı geçtigini yazdıran kod.

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Runtime.ExceptionServices;
+
+public class Student
+{
+    public static object Name { get; private set; }
+
+    static void Main()
+    {
+        var students = new List<(string Name, int FirstExam, int SecondExam, int OralExam)>();
+
+        double average;
+
+        Console.Write("Lütfen Öğrenci sayısını giriniz: ");
+        int studentNumber = int.Parse(Console.ReadLine());
+
+
+        for (int i = 0; i < studentNumber; i++)
+        {
+            Console.Write("İsim:");
+            string name = Console.ReadLine();
+
+            Console.Write("1. Yazılı: ");
+            int first = int.Parse(Console.ReadLine());
+
+            Console.Write("2. Yazılı: ");
+            int second = int.Parse(Console.ReadLine());
+
+            Console.Write("Sözlü: ");
+            int oral = int.Parse(Console.ReadLine());
+            
+            students.Add((name, first, second, oral));
+
+            average = (first + second + oral) / 3;
+
+            if (average >= 50)
+            {
+                Console.WriteLine("Geçti");
+            }
+
+            else
+            {
+                Console.WriteLine("Kaldı");
+            }
+
+        }
+
+        
+
+
+        Console.Read();
+    }
+}
